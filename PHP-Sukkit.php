981b1084760d11c89d4b62337f8f35a290582c91
EndermanbugZJFC <53002741+Endermanbugzjfc@users.noreply.github.com>
2021-10-24 06:32:20 +0800

    SUKKIT

--- PHP-Sukkit.php
+++ PHP-Sukkit.php
@@ -1,1 +1,15 @@
+echo "                     `. ___
+                    __,' __`.                _..----....____
+        __...--.'``;.   ,.   ;``--..__     .'    ,-._    _.-'
+  _..-''-------'   `'   `'   `'     O ``-''._   (,;') _,'
+,'________________                          \`-._`-','
+ `._              ```````````------...___   '-.._'-:
+    ```--.._      ,.                     ````--...__\-.
+            `.--. `-`                       ____    |  |`
+              `. `.                       ,'`````.  ;  ;`
+                `._`.        __________   `.      \'__/`
+                   `-:._____/______/___/____`.     \  `
+                               |       `._    `.    \
+                               `._________`-.   `.   `.___
+                                             SSt  `------'`\n\nPHP-SUKKITX\nThe divine, solar powered software designed for Minecraft: Bedrock Edition\nThis project is infpied by https://github.com/SukkitX";
 shell_exec("./PocketMine-MP.phar");
